f5c8e24b46767b878f0c431137bc56990d97e794
Alanna <alanna.noguchi@students.makeschool.com>
2020-06-07 18:01:25 -0700

    proper separation of concerns

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ const hbs = exphbs.create({
     handlebars: allowInsecurePrototypeAccess(handlebars),
 });
 
+
+// All routes were moved into controllers/events.js. They need to be imported here:
+require('./controllers/events')(app, models);  
+
 // The following line must appear AFTER const app = express() and before your routes!
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -41,63 +45,64 @@ var events = [
     { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://images.unsplash.com/photo-1565726166189-e9814a05ffde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" }
   ]
   
-// Index
-app.get('/', (req, res) => {
-    models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
-      res.render('events-index', { events: events });
-    })
-  })
+//  Add the index function into controllers/events.js: 
+// // Index
+// app.get('/', (req, res) => {
+//     models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
+//       res.render('events-index', { events: events });
+//     })
+//   })
 
-// NEW
-app.get('/events/new', (req, res) => {
-    res.render('events-new', {});
-  })
+// // NEW
+// app.get('/events/new', (req, res) => {
+//     res.render('events-new', {});
+//   })
   
-// SHOW
-app.get('/events/:id', (req, res) => {
-    // Search for the event by its id that was passed in via req.params
-    models.Event.findByPk(req.params.id).then((event) => {
-      // If the id is for a valid event, show it
-      res.render('events-show', { event: event })
-    }).catch((err) => {
-      // if they id was for an event not in our db, log an error
-      console.log(err.message);
-    })
-  })
+// // SHOW
+// app.get('/events/:id', (req, res) => {
+//     // Search for the event by its id that was passed in via req.params
+//     models.Event.findByPk(req.params.id).then((event) => {
+//       // If the id is for a valid event, show it
+//       res.render('events-show', { event: event })
+//     }).catch((err) => {
+//       // if they id was for an event not in our db, log an error
+//       console.log(err.message);
+//     })
+//   })
 
-// UPDATE
-app.put('/events/:id', (req, res) => {
-    models.Event.findByPk(req.params.id).then(event => {
-      event.update(req.body).then(event => {
-        res.redirect(`/events/${req.params.id}`);
-      }).catch((err) => {
-        console.log(err);
-      });
-    }).catch((err) => {
-      console.log(err);
-    });
-  });
+// // UPDATE
+// app.put('/events/:id', (req, res) => {
+//     models.Event.findByPk(req.params.id).then(event => {
+//       event.update(req.body).then(event => {
+//         res.redirect(`/events/${req.params.id}`);
+//       }).catch((err) => {
+//         console.log(err);
+//       });
+//     }).catch((err) => {
+//       console.log(err);
+//     });
+//   });
 
 
-// CREATE
-app.post('/events', (req, res) => {
-    models.Event.create(req.body).then(event => {
-      // Redirect to events/:id
-      res.redirect(`/events/${event.id}`)
+// // CREATE
+// app.post('/events', (req, res) => {
+//     models.Event.create(req.body).then(event => {
+//       // Redirect to events/:id
+//       res.redirect(`/events/${event.id}`)
   
-    }).catch((err) => {
-      console.log(err)
-    });
-  })
+//     }).catch((err) => {
+//       console.log(err)
+//     });
+//   })
 
-// EDIT
-app.get('/events/:id/edit', (req, res) => {
-    models.Event.findByPk(req.params.id).then((event) => {
-      res.render('events-edit', { event: event });
-    }).catch((err) => {
-      console.log(err.message);
-    })
-  });
+// // EDIT
+// app.get('/events/:id/edit', (req, res) => {
+//     models.Event.findByPk(req.params.id).then((event) => {
+//       res.render('events-edit', { event: event });
+//     }).catch((err) => {
+//       console.log(err.message);
+//     })
+//   });
 
 // DELETE
 app.delete('/events/:id', (req, res) => {
