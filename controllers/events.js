e8a220f8b27d83a282d65a50dcd061809ef10b91
Alanna <alanna.noguchi@students.makeschool.com>
2020-06-07 19:50:57 -0700

    added createdAt, footer, and bootstrap theme

--- controllers/events.js
+++ controllers/events.js
@@ -19,57 +19,60 @@ module.exports = function (app, models) {
     // SHOW
     app.get('/events/:id', (req, res) => {
         models.Event.findByPk(req.params.id, { include: [{ model: models.Rsvp }] }).then(event => {
+            let createdAt = event.createdAt;
+            createdAt = moment(createdAt).format('MMMM Do YYYY, h:mm:ss a');
+            event.createdAtFormatted = createdAt;
             res.render('events-show', { event: event });
         }).catch((err) => {
             console.log(err.message);
         })
     });
 
-    // // UPDATE
-    // app.put('/events/:id', (req, res) => {
-    //     models.Event.findByPk(req.params.id).then(event => {
-    //         event.update(req.body).then(event => {
-    //             res.redirect(`/events/${req.params.id}`);
-    //         }).catch((err) => {
-    //             console.log(err);
-    //         });
-    //     }).catch((err) => {
-    //         console.log(err);
-    //     });
-    // });
+    // UPDATE
+    app.put('/events/:id', (req, res) => {
+        models.Event.findByPk(req.params.id).then(event => {
+            event.update(req.body).then(event => {
+                res.redirect(`/events/${req.params.id}`);
+            }).catch((err) => {
+                console.log(err);
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+    });
 
 
-    // // CREATE
-    // app.post('/events', (req, res) => {
-    //     models.Event.create(req.body).then(event => {
-    //     // Redirect to events/:id
-    //         res.redirect(`/events/${event.id}`)
+    // CREATE
+    app.post('/events', (req, res) => {
+        models.Event.create(req.body).then(event => {
+        // Redirect to events/:id
+            res.redirect(`/events/${event.id}`)
     
-    //     }).catch((err) => {
-    //         console.log(err)
-    //     });
-    // });
+        }).catch((err) => {
+            console.log(err)
+        });
+    });
 
 
-    // // EDIT
-    // app.get('/events/:id/edit', (req, res) => {
-    //     models.Event.findByPk(req.params.id).then((event) => {
-    //         res.render('events-edit', { event: event });
-    //     }).catch((err) => {
-    //         console.log(err.message);
-    //     })
-    // });
+    // EDIT
+    app.get('/events/:id/edit', (req, res) => {
+        models.Event.findByPk(req.params.id).then((event) => {
+            res.render('events-edit', { event: event });
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    });
 
 
-    // // DELETE
-    // app.delete('/events/:id', (req, res) => {
-    //     models.Event.findByPk(req.params.id).then(event => {
-    //         event.destroy();
-    //         res.redirect(`/`);
-    //     }).catch((err) => {
-    //         console.log(err);
-    //     });
-    // });
+    // DELETE
+    app.delete('/events/:id', (req, res) => {
+        models.Event.findByPk(req.params.id).then(event => {
+            event.destroy();
+            res.redirect(`/`);
+        }).catch((err) => {
+            console.log(err);
+        });
+    });
 
 }
 
